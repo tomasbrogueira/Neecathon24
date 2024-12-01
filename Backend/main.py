052b318fb78da3a6aa27440913fd03f77ead23c0
João Guilherme <joaommguilherme@tecnico.ulisboa.pt>
2024-12-01 01:08:43 +0000

    added integration

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,12 @@
-import flask 
+import flask
+from flask import jsonify
+import threading
 
 app = flask.Flask(__name__)
 
+# Shared variables
+shared_data = {"drowsy_ratio": 0.0}  # Initial drowsy ratio
+lock = threading.Lock()
 alarm_on = False
 bpm_list = []
 
@@ -11,45 +16,29 @@ def home():
 
 @app.route("/check_alarm", methods=["GET"])
 def check_alarm():
-    """
-    Checks the status of the buzzer by sending a GET request to the specified URL.
-    
-    Returns:
-        dict: Parsed JSON response if successful, None otherwise.
-    """
-
     global alarm_on
-    
-    return flask.jsonify({"status": "OK", "alarm_on": alarm_on})
+    return jsonify({"status": "OK", "alarm_on": alarm_on})
 
 @app.route("/toggle_alarm", methods=["GET"])
 def toggle_alarm():
-    """
-    Toggles the buzzer on or off by sending a POST request to the specified URL.
-    
-    Returns:
-        dict: Parsed JSON response if successful, None otherwise.
-    """
-
     global alarm_on
-
     alarm_on = not alarm_on
-    return flask.jsonify({"status": "OK", "alarm_on": alarm_on})
+    return jsonify({"status": "OK", "alarm_on": alarm_on})
 
 @app.route("/add_bpm", methods=["POST"])
 def add_bpm():
-    """
-    Adds the BPM value to the database.
-    
-    Returns:
-        dict: Parsed JSON response if successful, None otherwise.
-    """
-
     bpm = flask.request.json.get("bpm")
     bpm_list.append(bpm)
-    
+    return jsonify({"status": "OK"})
 
-    return flask.jsonify({"status": "OK"})
+@app.route("/get_drowsy_ratio", methods=["GET"])
+def get_drowsy_ratio():
+    """
+    Returns the current drowsy ratio value.
+    """
+    with lock:
+        current_ratio = shared_data["drowsy_ratio"]
+    return jsonify({"status": "OK", "drowsy_ratio": current_ratio})
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000)
